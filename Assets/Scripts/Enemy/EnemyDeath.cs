9b9a63fde9dd06da874afc5209819414fd04b2da
parahunter <parahunter@gmail.com>
2015-04-20 00:02:33 +0100

    death anims

--- Assets/Scripts/Enemy/EnemyDeath.cs
+++ Assets/Scripts/Enemy/EnemyDeath.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 
 public class EnemyDeath : MonoBehaviour 
 {
+    public Transform enemyDeath;
 
     public void OnLaserHit()
     {
+        Instantiate(enemyDeath, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/Player/PlayerDeath.cs
+++ Assets/Scripts/Player/PlayerDeath.cs
@@ -6,10 +6,12 @@ public class PlayerDeath : MonoBehaviour
 {
     public Camera mainCamera;
     public GameManager gameManager;
+    public Transform playerDeath;
 
     public void OnLaserHit()
     {
         mainCamera.transform.parent = null;
+        Instantiate(playerDeath, transform.position, Quaternion.identity);
 
         Destroy(gameObject);
 
